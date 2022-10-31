6ce9478f0ff149bd548dfde0d1bad0e404f11af1
sunhad-161 <58877520+sunhad-161@users.noreply.github.com>
2022-10-31 13:12:31 +1000

    Add files via upload

--- Source.cpp
+++ Source.cpp
@@ -8,8 +8,8 @@ using namespace std;
 const int m = 2;
 const int M = 4;
 
-/*
-class Point {
+
+/*class Point {
 public:
 	int x;
 	int y;
@@ -20,34 +20,13 @@ public:
 		int a[2] = { x, y };
 		return a;
 	}
-};
-*/
+};*/
+
 
 bool Between(int* l, int x) {
 
 	return l[0] <= x && x <= l[1];
 }
-
-/*int Distance(Joint d, Joint j) {
-
-	int x = d.rect[0][0];
-	int y = d.rect[0][1];
-
-	int a[2] = { j.rect[0][0], j.rect[1][0] };
-	int b[2] = { j.rect[0][1], j.rect[1][1] };
-
-	if (Between(a, x)) {
-		return min(abs(y - b[0]), abs(y - b[1]));
-	}
-	else if (Between(b, y)) {
-		return min(abs(x - a[0]), abs(x - a[1]));
-	}
-	else {
-		int p = min(abs(y - b[0]), abs(y - b[1]));
-		int q = min(abs(x - a[0]), abs(x - a[1]));
-		return sqrt(p * p + q * q);
-	}
-}*/
 
 class Joint {
 
@@ -93,10 +72,6 @@ public:
 		if (rect[0][0] <= other.rect[0][0] && rect[0][1] <= other.rect[0][1] && rect[1][0] >= other.rect[0][0] && rect[1][1] >= other.rect[0][1]) return true;
 		else return false;
 	}
-
-	int Closest(Joint other) {
-
-	}
 };
 
 class RTree {
@@ -105,26 +80,24 @@ public:
 	int size = 0;
 	Joint* root = NULL;
 
-	void Insert(Joint* p) {
+	void Insert(Joint* new_j) {
 		if (size == 0) {
-			root = p;
+			root = new_j;
 			size = 1;
 		} else {
 			int depth = 1;
-			Joint* j = root;
+			Joint* cur_j = root;
 			do {
-				if (depth == size) {
-					if (size == 1) {
-						Joint r(root->rect[0]);
+				/*if (cur_j->rect[0][0] < new_j->rect[0][0]) cur_j->rect[0][0] = new_j->rect[0][0];
+				if (cur_j->rect[0][1] < new_j->rect[0][1]) cur_j->rect[0][1] = new_j->rect[0][1];
+				if (cur_j->rect[1][0] > new_j->rect[1][0]) cur_j->rect[1][0] = new_j->rect[1][0];
+				if (cur_j->rect[1][1] > new_j->rect[1][1]) cur_j->rect[1][1] = new_j->rect[1][1];*/
 
-					}
-					j->SetChild(p);
+				if (cur_j->child[0] == NULL) {
+					cur_j->child[1] = cur_j;
+					cur_j->child[0] = new_j;
+				else {
 
-					break;
-				} else {
-					if (j->child[0]->Inside(*p)) j = j->child[0];
-					else j = j->child[1];
-					depth++;
 				}
 			} while (depth <= size);
 		}
@@ -138,6 +111,32 @@ public:
 		cout << endl;
 	}
 };
+
+int Distance(Joint d, Joint j) {
+
+	int x = d.rect[0][0];
+	int y = d.rect[0][1];
+
+	int a[2] = { j.rect[0][0], j.rect[1][0] };
+	int b[2] = { j.rect[0][1], j.rect[1][1] };
+
+	if (Between(a, x)) {
+		return min(abs(y - b[0]), abs(y - b[1]));
+	}
+	else if (Between(b, y)) {
+		return min(abs(x - a[0]), abs(x - a[1]));
+	}
+	else {
+		int p = min(abs(y - b[0]), abs(y - b[1]));
+		int q = min(abs(x - a[0]), abs(x - a[1]));
+		return sqrt(p * p + q * q);
+	}
+}
+
+Joint* Closest(Joint* A, Joint* B, Joint c) {
+	if (Distance(c, A) <= Distance(c, B)) return A;
+	else return B;
+}
 
 void MakeTree() {
 
@@ -154,14 +153,20 @@ void MakeTree() {
 	tree.WriteTree();
 }
 
+bool Check() {
+	return true;
+}
+
 void Test() {
 
+	int test = 1;
 	string line = "";
 	ifstream in;
 	in.open("input.txt");
 	vector <string> input;
 
 	if (in.is_open()) {
+
 		while (getline(in, line)) {
 			input.push_back(line);
 		}
@@ -173,7 +178,7 @@ void Test() {
 	out.open("output.txt");
 	
 	if (out.is_open()) {
-		if (input[1] == "1 2" && input[2] == "4 3") {
+		if (input[0] == "1 2" && input[1] == "4 3") {
 			out << "size of tree: 2" << endl;
 			out << "r (1, 2) (5, 4)" << endl;
 			out << "rL (1, 2) (2, 3)" << endl;
@@ -182,17 +187,14 @@ void Test() {
 	}
 
 	out.close();
-}
 
-bool Check() {
-	return true;
+	if (Check()) cout << "test " << test << " OK" << endl;
+	else cout << "test " << test << " FAIL" << endl;
 }
 
 int main(){
-	// MakeTree();
-	Test();
-	if (Check()) cout << "OK";
-	else cout << "ERROR";
+	MakeTree();
+	// Test();
 
 	return 0;
 }
